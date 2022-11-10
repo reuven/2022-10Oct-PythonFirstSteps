2825fa993211bfdad5e28cf7fd00d695c2605324
Reuven Lerner <reuven@lerner.co.il>
2022-11-10 21:27:25 +0200

    gitautosync update 56

--- temperatures.py
+++ temperatures.py
@@ -3,8 +3,12 @@
 # 2. The return value will be the converted temperature.
 # 3. import this module, and show that you can convert temperatures in either direction.
 
-print(f'Hello from {__name__}!')
-print(f'Now in file: {__file__}')
+# this line means: Only print __name__ and __file__ when the program is being run standalone.
+# if we were imported as a module, don't print anything.
+
+if __name__ == '__main__':
+    print(f'Hello from {__name__}!')
+    print(f'Now in file: {__file__}')
 
 def c2f(c_temp):
     return c_temp * 1.8 + 32.00
@@ -12,4 +16,5 @@ def c2f(c_temp):
 def f2c(f_temp):
     return (f_temp - 32) / 1.8 
 
-print(f'Goodbye from {__name__}!')
+if __name__ == '__main__':
+    print(f'Goodbye from {__name__}!')
