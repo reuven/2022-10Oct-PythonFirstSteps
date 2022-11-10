bfa07409e93ab938dd04a7419ac6310aba5db7ee
Reuven Lerner <reuven@lerner.co.il>
2022-11-10 21:30:30 +0200

    gitautosync update 58

--- temperatures.py
+++ temperatures.py
@@ -6,9 +6,9 @@
 # this line means: Only print __name__ and __file__ when the program is being run standalone.
 # if we were imported as a module, don't print anything.
 
-if __name__ == '__main__':
-    print(f'Hello from {__name__}!')
-    print(f'Now in file: {__file__}')
+# if __name__ == '__main__':
+#     print(f'Hello from {__name__}!')
+#     print(f'Now in file: {__file__}')
 
 def c2f(c_temp):
     return c_temp * 1.8 + 32.00
@@ -17,4 +17,13 @@ def f2c(f_temp):
     return (f_temp - 32) / 1.8 
 
 if __name__ == '__main__':
-    print(f'Goodbye from {__name__}!')
+    n = input('Enter degrees: ').strip()
+    f_or_c = input('Enter F or C: ').strip()
+    
+    if f_or_c == 'F':
+        print(f'{n} degrees F -> {f2c(n)} C')
+    elif f_or_c == 'C':
+        print(f'{n} degrees C -> {f2c(n)} F')
+    else:
+        print(f'Unknown scale {f_or_c}'
+        
