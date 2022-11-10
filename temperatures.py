c2cc3db9164c1447cab1fb5c150f97544c63fb5f
Reuven Lerner <reuven@lerner.co.il>
2022-11-10 20:51:48 +0200

    gitautosync update 42

---
+++ temperatures.py
@@ -0,0 +1,11 @@
+# 1. Create a new module, `convert_temp.py`, containing two functions, `c2f` and `f2c`, which convert from Celsius to # 
+# Fahrentheight (or back).  The functions assume that they get a single number as input.
+# 2. The return value will be the converted temperature.
+# 3. import this module, and show that you can convert temperatures in either direction.
+
+def c2f(c_temp):
+    return c_temp * 1.8 + 32.00
+
+def f2c(f_temp):
+    return (f_temp - 32) / 1.8 
+
